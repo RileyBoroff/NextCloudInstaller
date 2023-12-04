fc58a8e9838d258007259a91ba9d654bea2fa031
RileyBoroff <rboroff72@gmail.com>
2023-12-04 10:23:07 -0500

    php 8 config

--- Installer.sh
+++ Installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Enable basic error debugging
 PS4='+${LINENO}: '
-set -x
+set -u
 
 # Basic Nextcloud installer for Raspberry Pi
 
@@ -12,12 +12,15 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Update and upgrade system
-echo "Updating the system"
+echo "Adding php 8.0 repo and updating the system"
+wget -qO /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+
 apt update && apt upgrade -y
 
-# Install required packages
+# Install required packages including PHP 8.0
 echo "Installing pre-reqs"
-apt install -y apache2 mariadb-server libapache2-mod-php php-gd php-json php-mysql php-curl php-mbstring php-intl php-imagick php-xml php-zip
+apt install -y apache2 mariadb-server libapache2-mod-php8.0 php8.0-gd php8.0-json php8.0-mysql php8.0-curl php8.0-mbstring php8.0-intl php8.0-imagick php8.0-xml php8.0-zip
 
 # Restart Apache
 echo "Restarting the Apache service"
